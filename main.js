e681c605cf00fe086c38c847b23ac0a7fd4d38c6
vallemedleys <140564514+vallemedleys@users.noreply.github.com>
2023-08-02 18:37:17 -0500

    made menu navbar

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import './style.css'
 import './index.html'
 
+function toggleMenu() {
+    var menu = document.getElementById('mobile-nav');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+  }
+  
