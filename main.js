95233e2321160a1b8917a500053dc643874f8d9f
vallemedleys <140564514+vallemedleys@users.noreply.github.com>
2023-08-02 18:45:25 -0500

    fixing toggle menu

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ import './style.css'
 import './index.html'
 
 function toggleMenu() {
-    var menu = document.getElementById('mobile-nav');
-    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+    var mobileNav = document.getElementById('mobile-nav');
+    if (mobileNav.style.display === 'block') {
+      mobileNav.style.display = 'none';
+    } else {
+      mobileNav.style.display = 'block';
+    }
   }
+  
+  // If you still want to add the event listener, you can do so like this:
+  document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
+  
   
